0d6c78165cbb187d8c26716a654452b22ecac146
JeisonJimenezA <113473031+JeisonJimenezA@users.noreply.github.com>
2025-10-29 17:04:24 +0000

    Deploying to gh-pages from @ VerbaNexAI/verbanexai.github.io@1f8b6b7140ef5efd7d614194c92a90d17476fe07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,8 +426,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-we-participated-in-iberlef-2025-our-presentations-are-available-in-the-publication-section",
-          title: 'We participated in IberLEF 2025. Our presentations are available in the publication section...',
+            },},{id: "news-we-participated-in-iberlef-2025-iberian-languages-evaluation-forum-our-presentations-are-available-in-the-publication-section",
+          title: 'We participated in IberLEF 2025 (Iberian Languages Evaluation Forum). Our presentations are available...',
           description: "",
           section: "News",},{id: "news-we-participated-in-clef-2025-conference-and-labs-of-the-evaluation-forum-our-presentations-are-available-in-the-publication-section",
           title: 'We participated in CLEF 2025 (Conference and Labs of the Evaluation Forum). Our...',
